2a795a1d998c55e496382e089ac6eb39b0146f13
John Malloy <jfmalloy1@gmail.com>
2020-06-16 16:34:54 -0700

    Fragment coocurrance matrix added

--- Word2Vect/build_cooccurance_matrix.py
+++ Word2Vect/build_cooccurance_matrix.py
@@ -2,6 +2,7 @@ import pandas as pd
 from rdkit import Chem
 import numpy as np
 import re
+import itertools
 
 """ Return a dataframe of all KEGG data """
 """ Input: the filepath (fp) to a csv file containing canonical smiles strings """
@@ -24,17 +25,15 @@ def get_chem_fragments(fp):
 """ Determine the presence of a chemical fragment within a smiles string """
 """ Input: smiles string of a chemical, fragment of a molecule in SMARTS format """
 """ Output: T/F (presence/absence) """
-def find_single_presence(smiles_mol, f1_mol, f2_mol):
-    try:
-        return (smiles_mol.HasSubstructMatch(f1_mol) and smiles_mol.HasSubstructMatch(f2_mol))
-    except:
-        return False
+#def find_single_presence(smiles_mol, f1_mol, f2_mol):
+def find_single_presence(f1_occur, f2_occur):
+    return (f1_occur and f2_occur)
 
 def main():
     #Read in KEGG data
     df = get_KEGG_smiles("kegg_data.csv")
     #Read in chem fragments
-    fragments = get_chem_fragments("functional_groups_smarts.txt")
+    fragments = get_chem_fragments("frags.txt")
 
     #Co-occurance dataframe
     co_df = pd.DataFrame(0, index=fragments, columns=fragments)
@@ -46,25 +45,40 @@ def main():
 
     #Loop through all fragments to find co-occurances
     count = 0
-    for f1 in fragments:
+    occurance_dict = {}
+    for f in fragments:
         count += 1
-        print(f1, count, "of 314")
-        #Mol structure for initial comparison fragment
-        frag1_mol = Chem.MolFromSmarts(f1)
-        #Compare each fragment with each other
-        for f2 in fragments:
-            #2nd fragment Mol structure
-            frag2_mol = Chem.MolFromSmarts(f2)
-            #Loop through all KEGG cpds
-            for cpd in KEGG_mol_dict:
-                if find_single_presence(KEGG_mol_dict[cpd], frag1_mol, frag2_mol):
-                    #If true, add one to dataframe count
-                    co_df.loc[f1][f2] += 1
+        print(count, "of", len(fragments))
+        cpd_dict = {}
+        for cpd in KEGG_mol_dict:
+            try:
+                cpd_dict[cpd] = KEGG_mol_dict[cpd].HasSubstructMatch(Chem.MolFromSmarts(f))
+            except:
+                cpd_dict[cpd] = False
+        occurance_dict[f] = cpd_dict
 
+    count = 0
+    percent = -1
+    #Get all pairs
+    frag_combos = list(itertools.combinations(fragments, 2))
+    l = len(frag_combos)
+    for pair in frag_combos:
+        #Output progress
+        count += 1
+        p = int(count / float(l) * 100)
+        if p > percent:
+            percent = p
+            print(percent, "completed!")
 
+        #Build co-occurance matrix
+        for cpd in KEGG_mol_dict:
+            if find_single_presence(occurance_dict[pair[0]][cpd], occurance_dict[pair[1]][cpd]):
+                #If true, add one to dataframe count
+                co_df.loc[pair[0]][pair[1]] += 1
+                co_df.loc[pair[1]][pair[0]] += 1
 
     print(co_df)
-    co_df.to_csv("functional_groups_coocurrance.csv")
+    co_df.to_csv("frags_coocurrance.csv")
 
 if __name__ == "__main__":
     main()
