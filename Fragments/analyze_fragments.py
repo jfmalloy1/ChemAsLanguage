8ac2ef2cbfafbbdcaa8a765eb7fac5ef4e191b8d
John Malloy <jfmalloy1@gmail.com>
2021-02-24 16:26:27 -0700

    Network creation test

--- Fragments/analyze_fragments.py
+++ Fragments/analyze_fragments.py
@@ -3,6 +3,9 @@ import pandas as pd
 from tqdm import tqdm
 from scipy import stats
 import pickle
+import networkx as nx
+from itertools import combinations
+import matplotlib.pyplot as plt
 
 """ Get smiles strings for a set of compounds
     Input: list of cpd ids for a given set of compounds
@@ -35,34 +38,65 @@ def convert_to_mol_smarts(smarts):
 """
 def find_frags_in_cpds(cpd, frags):
     frag_count = 0
+    frags_in_cpd = []
     for f in frags:
         if cpd.HasSubstructMatch(f):
             frag_count += 1
-    return frag_count
+            frags_in_cpd.append(Chem.MolToSmiles(f))
+    return frag_count, frags_in_cpd
+
+""" Create a networkx graph of fragments. Fragments are connected if they are within the same compound
+    Input: List of lists, each sublist are fragments within a single archaea compound
+    Output: A networkx graph of fragments
+"""
+def create_graph(frags):
+    G = nx.Graph()
+    #TEST: create a network of fragments from the 0th compound
+    for cpd in tqdm(frags):
+        for f in cpd:
+            G.add_node(f)
+        G.add_edges_from(combinations(cpd,2))
+    print(nx.info(G))
+    return G
+    # #Try drawing it
+    # plt.subplot(121)
+    # nx.draw(G, with_labels=True, font_weight="bold")
+    # plt.show()
 
 def main():
-    #Read in fragments (Archaea as a test)
-    fp = "Biology/Data/Archaea/Archaea0_fullOccurances.csv"
-    a_df = pd.read_csv(fp)
-    a_frags = a_df["Frags"].tolist() #Smarts strings found in archaea
-    a_frags = convert_to_mol_smarts(a_frags)
-    print("Found", len(a_frags), "archaea fragments")
+    ### Number of fragments in each compound ###
+    # #Read in fragments (Archaea as a test)
+    # fp = "Biology/Data/Archaea/Archaea0_fullOccurances.csv"
+    # a_df = pd.read_csv(fp)
+    # a_frags = a_df["Frags"].tolist() #Smarts strings found in archaea
+    # a_frags = convert_to_mol_smarts(a_frags)
+    # print("Found", len(a_frags), "archaea fragments")
+    #
+    # #Archaea smiles
+    # fp = "Biology/Data/archaea_cpds.csv"
+    # a_cpds = get_smiles(fp) #Smiles strings of archaea cpds
+    # a_cpds = [c for c in a_cpds if str(c) != 'nan'] #Remove nans
+    # a_cpds = convert_to_mol_smiles(a_cpds)
+    # print("Found", len(a_cpds), "archea compounds")
+    #
+    # #Find number of fragments in each compounds
+    # frag_count = []
+    # frags_in_all_cpds = []
+    # for cpd in tqdm(a_cpds):
+    #     count, frags_in_cpd = find_frags_in_cpds(cpd, a_frags)
+    #     frag_count.append(count)
+    #     frags_in_all_cpds.append(frags_in_cpd)
+    #
+    # #Basic descriptive statistics
+    # print(stats.describe(frag_count))
+    # pickle.dump(frag_count, open("Biology/Data/archaea_fragment_counts.p", "wb"))
+    # pickle.dump(frags_in_all_cpds, open("Biology/Data/archaea_listOfAllFragsPerCpd.p", "wb"))
 
-    #Archaea smiles
-    fp = "Biology/Data/archaea_cpds.csv"
-    a_cpds = get_smiles(fp) #Smiles strings of archaea cpds
-    a_cpds = [c for c in a_cpds if str(c) != 'nan'] #Remove nans
-    a_cpds = convert_to_mol_smiles(a_cpds)
-    print("Found", len(a_cpds), "archea compounds")
+    ### Network of Compounds ###
+    frags_in_all_cpds = pickle.load(open("Biology/Data/archaea_listOfAllFragsPerCpd.p", "rb"))
 
-    #Find number of fragments in each compounds
-    frag_count = []
-    for cpd in tqdm(a_cpds):
-        frag_count.append(find_frags_in_cpds(cpd, a_frags))
+    pickle.dump(create_graph(frags_in_all_cpds), open("Biology/Data/archea_frag_network.p", "wb"))
 
-    #Basic descriptive statistics
-    print(stats.describe(frag_count))
-    pickle.dump(frag_count, open("Biology/Data/fragment_counts.p", "wb"))
 
 
 if __name__ == "__main__":
